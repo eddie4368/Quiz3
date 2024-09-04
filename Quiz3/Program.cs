f6d690079a778e6b552572e4056df1f5627919af
eddie ortega <23edortega@gmail.com>
2024-09-04 12:59:23 -0500

    Add project files.

---
+++ Quiz3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] word = { "hello", "racecar", "Exit" }; //declares the word from thestring
+            Console.WriteLine("Enter a string:");// ask for user input
+            Console.ReadLine();
+            
+
+            Console.WriteLine("Enter a string:");// ask for user input
+            Console.ReadLine();
+
+            Console.WriteLine("Enter a string:");// ask for user input
+            Console.ReadLine();
+        }
+    }
+}
